4990f27e8ce7110f7ac9306447d16ddaff1dc78d
rtaylor02 <taylor.rod.j@gmail.com>
2022-10-12 13:42:33 +0100

    adding structure and some basic pages

---
+++ meal-tracker-app-v1/src/ingredients/AddIngredientPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const AddIngredientPage = () => (
+    <h1>Add Ingredient page</h1>
+);
---
+++ meal-tracker-app-v1/src/recipes/RecipeSearchPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const RecipeSearchPage = () => (
+    <h1>Recipe Search page</h1>
+);
